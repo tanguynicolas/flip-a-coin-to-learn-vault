d4cdfa5b010a8a0744b9483003bf4cb4f5aaa6ad
Tanguy NICOLAS <tanguy-nicolas@outlook.fr>
2024-02-26 16:38:38 +0100

    feat: add postgres integration

--- src/CoinToFlip/settings.py
+++ src/CoinToFlip/settings.py
@@ -44,7 +44,6 @@ env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -105,8 +104,12 @@ WSGI_APPLICATION = 'CoinToFlip.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'flip-a-coin',
+        'USER': 'user',
+        'PASSWORD': 'user',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
