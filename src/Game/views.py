6167ec976eb02418389e747e814ac7165d5534da
Tanguy NICOLAS <tanguy-nicolas@outlook.fr>
2024-02-27 18:32:06 +0100

    feat: initialize counter with database

--- src/Game/views.py
+++ src/Game/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 
+from .models import Result
+
 # Create your views here.
 def homepage(request):
-    return render(request, "game/index.html")
+    # Récupérer le nombre actuel de "pile" et "face" depuis la base de données
+    result = Result.objects.first()  # Supposant qu'il n'y ait qu'une seule entrée dans la table Result
+
+    # Si aucune entrée n'existe, crée une nouvelle instance
+    if not result:
+        result = Result.objects.create(heads=0, tails=0)
+
+    # Passer les valeurs à la page HTML
+    context = {
+        'heads_count': result.heads,
+        'tails_count': result.tails,
+    }
+
+    return render(request, "game/index.html", context)
